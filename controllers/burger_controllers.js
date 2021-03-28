8b854948d814ac21328e3c52f4dad2e0b56b40b2
Esteban Meza <meza.19@osu.edu>
2021-03-28 18:52:52 -0400

    tried to figure out button commands

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -4,11 +4,12 @@ const burger = require("../models/burger.js");
 
 // Create all routes and set up logic within those routes where required
 router.get("/", function(req, res) {
-    burgers.all(function(data)  {
+    burger.all(function(data)  {
         var hbsObject = {
             burgers: data
         };
         console.log(hbsObject);
+        res.render("index", hbsObject);
     } );
 });
 
@@ -25,11 +26,11 @@ router.post("/api/burgers", function (req,res) {
 
 router.put("/api/burgers/:id", function(req, res) {
     var devoured = "id = " + req.params.id;
-    console.log("condition", devoured);
+    console.log("devoured", devoured);
 
     burger.update({
         devoured: 1
-    }, condition, function(result) {
+    }, devoured, function(result) {
         if (result.changedRows == 0) {
             "If no rows were changed, then the ID does not exist"
             return res.status(404).end();
